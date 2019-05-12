f3de9ad0d37e407dfbf7af2fb33036017605abb5
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-05-12 14:23:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2049,6 +2049,17 @@ msg.delete();
 }
 });
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Type ?help | #♛ ExPlosive ♛`,"http://twitch.tv/Death Shop")
+client.user.setStatus("online")
+});
+
 
 client.on('message', message => {
       if(message.content.startsWith ("-marry")) {
